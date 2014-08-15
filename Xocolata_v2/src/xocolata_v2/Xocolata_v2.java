8ab9e358526c33bdde1aaa37e0156fc875b69b9d
Marco Barrios <mabarriosmarco@gmail.com>
2014-08-14 21:33:33 -0600

    Creacion de Proyecto Xocolata v2

---
+++ Xocolata_v2/src/xocolata_v2/Xocolata_v2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package xocolata_v2;
+
+/**
+ *
+ * @author Marco
+ */
+public class Xocolata_v2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Xocolata Version 2");
+    }
+    
+}
