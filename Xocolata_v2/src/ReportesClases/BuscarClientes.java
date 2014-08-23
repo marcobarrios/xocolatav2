f73354316daa6df3d119081106741a73bad114da
Gerson <gerson1vime@gmail.com>
2014-08-23 11:23:55 -0600

    Se agrego los reportes de Datos de Clientes y Vendedores2

---
+++ Xocolata_v2/src/ReportesClases/BuscarClientes.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ReportesClases;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import xocolata_v2.ConexionDB;
+
+/**
+ *
+ * @author Gerson
+ */
+public class BuscarClientes {
+
+  
+    public BuscarClientes() {
+        
+    }
+    
+    public void llenarNombres(){
+        String[] titulos={"Nombres Clientes"};
+        Object[] registros = new Object[1];
+        DefaultTableModel model= new DefaultTableModel(null,titulos){public boolean isCellEditable(int rowIndex,int columnIndex){return false;} };
+        Connection conexion = ConexionDB.ObtenerConexion(); 
+        if(conexion!=null)
+        {
+            try
+            {
+                Statement Query = conexion.createStatement();            
+                ResultSet Datos = Query.executeQuery("select nombreCliente from tblClientes order by nombreCliente");
+                Datos = Query.getResultSet();                    
+                while (Datos.next()) 
+                {
+                    registros[0]=Datos.getString("nombreCliente");
+                    model.addRow(registros);                  
+                }       
+                VentanaDatosClientes.tblNombres.setModel(model);
+            }
+            catch (SQLException exp) 
+            {
+                JOptionPane.showMessageDialog(null, "No Existe ningún Cliente");
+            }
+        }
+    }
+    
+    public void llenarFormulario(String nombre){
+        Connection conexion = ConexionDB.ObtenerConexion();
+         try
+            {
+                 Statement Query = conexion.createStatement();
+                 ResultSet dato = Query.executeQuery("select nombreCliente, DPI, direccionCliente, telefonoCliente from tblClientes where nombreCliente = '" + nombre + "'");
+                 dato = Query.getResultSet();
+                while (dato.next()) 
+                {
+                    VentanaDatosClientes.nombre.setText(dato.getString("nombreCliente"));
+                    VentanaDatosClientes.dpi.setText(dato.getString("DPI"));
+                    VentanaDatosClientes.direccion.setText(dato.getString("direccionCliente"));
+                    VentanaDatosClientes.telefono.setText(dato.getString("telefonoCliente"));
+                }
+                 dato.close();
+                 conexion.close();
+                 
+             }
+             catch (SQLException ex)
+             {
+                 JOptionPane.showMessageDialog(null, "Error");
+             }
+    }
+    
+    
+}
---
+++ Xocolata_v2/src/ReportesClases/BuscarVendedores.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ReportesClases;
+
+import ContenedorComboBox.Item;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import xocolata_v2.ConexionDB;
+
+/**
+ *
+ * @author Gerson
+ */
+public class BuscarVendedores {
+
+  
+    public BuscarVendedores() {
+        
+    }
+    
+    public void llenarNombres(){
+        String[] titulos={"Nombres Vendedores"};
+        Object[] registros = new Object[1];
+        DefaultTableModel model= new DefaultTableModel(null,titulos){public boolean isCellEditable(int rowIndex,int columnIndex){return false;} };
+        Connection conexion = ConexionDB.ObtenerConexion(); 
+        if(conexion!=null)
+        {
+            try
+            {
+                Statement Query = conexion.createStatement();            
+                ResultSet Datos = Query.executeQuery("select nombreVendedor from tblvendedores order by nombreVendedor");
+                Datos = Query.getResultSet();                    
+                while (Datos.next()) 
+                {
+                    registros[0]=Datos.getString("nombreVendedor");
+                    model.addRow(registros);                  
+                }       
+                VentanaDatosVendedores.tblNombres.setModel(model);
+            }
+            catch (SQLException exp) 
+            {
+                JOptionPane.showMessageDialog(null, "No Existe ningún Vendedor");
+            }
+        }
+    }
+    
+    public void llenarFormulario(String nombre){
+        Connection conexion = ConexionDB.ObtenerConexion();
+         try
+            {
+                 Statement Query = conexion.createStatement();
+                 ResultSet dato = Query.executeQuery("select codigoVendedor, nombreVendedor, DPI, direccionVendedor from tblvendedores where nombreVendedor = '" + nombre + "'");
+                 dato = Query.getResultSet();
+                while (dato.next()) 
+                {
+                    VentanaDatosVendedores.codigo.setText(dato.getString("codigoVendedor"));
+                    VentanaDatosVendedores.nombre.setText(dato.getString("nombreVendedor"));
+                    VentanaDatosVendedores.dpi.setText(dato.getString("DPI"));
+                    VentanaDatosVendedores.direccion.setText(dato.getString("direccionVendedor"));
+                }
+                 dato.close();
+                 conexion.close();
+                 llenarTelefonos(nombre);
+             }
+             catch (SQLException ex)
+             {
+                 JOptionPane.showMessageDialog(null, "Error");
+             }
+    }
+    
+    private void llenarTelefonos(String nombre){
+        String[] titulos={"Teléfono(s)"};
+        Object[] registros = new Object[1];
+        DefaultTableModel model= new DefaultTableModel(null,titulos){public boolean isCellEditable(int rowIndex,int columnIndex){return false;} };
+        Connection conexion = ConexionDB.ObtenerConexion(); 
+        if(conexion!=null)
+        {
+            try
+            {
+                Statement Query = conexion.createStatement();            
+                ResultSet Datos = Query.executeQuery("select contacto from tblContactoVendedor\n" +
+                                                     "inner join tblVendedores on tblContactoVendedor.idVendedor = tblVendedores.idVendedor\n" +
+                                                     "where tblVendedores.nombreVendedor = '" + nombre + "'");
+                Datos = Query.getResultSet();                    
+                while (Datos.next()) 
+                {
+                    registros[0]=Datos.getString("contacto");
+                    model.addRow(registros);                  
+                }       
+                VentanaDatosVendedores.tblTelefonos.setModel(model);
+            }
+            catch (SQLException exp) 
+            {
+                JOptionPane.showMessageDialog(null, "No Existe ningún Vendedor");
+            }
+        }
+    }
+}
